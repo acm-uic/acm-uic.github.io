eda8a89dea58dbcdaed8a16de012fd4cde4a98dd
dhelle2 <dhelle2@uic.edu>
2021-08-11 01:48:15 -0500

    changed button to add "Introduction" so peopke know where to go for a description of uic acm and more info about what we do.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
             className="button button--secondary button--lg"
             to="/docs/introduction"
           >
-            Documentation ✏
+            Introduction/Documentation ✏
           </Link>
         </div>
       </div>
