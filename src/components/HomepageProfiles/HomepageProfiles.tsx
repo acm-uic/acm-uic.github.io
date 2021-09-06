adcf6e7cf84ecfc8fb36a1f5ab304a26caffdc39
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2021-09-06 13:54:40 -0700

    ⚡ optimize image sizes and 🧹 cleanup css (#55)
    * optimize image sizes and cleanup css
    
    * Update HomepageProfiles.tsx
    
    * update cspell words

--- src/components/HomepageProfiles/HomepageProfiles.tsx
+++ src/components/HomepageProfiles/HomepageProfiles.tsx
@@ -1,25 +1,23 @@
 import React from "react";
-import clsx from "clsx";
-import styles from "./HomepageProfiles.module.css";
 
 export type HomepageOfficerProps = Record<string, never>;
 
 const officers = [
   {
     name: "Christian Bingman",
-    image: "https://avatars.githubusercontent.com/u/42191425?s=200&v=4",
-    url: "https://github.com/ChristianBingman",
+    image: "https://git.christianbingman.com/avatar/19c1437930a1e9db7482a581fc45f60a?size=100",
+    url: "https://git.christianbingman.com/ChristianBingman",
     description: <>President</>,
   },
   {
     name: "Anastasiia Evdokimova",
-    image: "https://avatars.githubusercontent.com/u/36254266?s=200&v=4",
+    image: "https://avatars.githubusercontent.com/u/36254266?s=100&v=4",
     url: "https://github.com/SkylinSky",
     description: <>Vice President</>,
   },
   {
     name: "Kalpkumar Shah",
-    image: "https://avatars.githubusercontent.com/u/76023884?s=200&v=4",
+    image: "https://avatars.githubusercontent.com/u/76023884?s=100&v=4",
     url: "https://github.com/kalps358",
     description: <>Treasurer</>,
   },
@@ -28,7 +26,7 @@ const officers = [
 const sysadmins = [
   {
     name: "Bharat Middha",
-    image: "https://avatars.githubusercontent.com/u/5100938?s=200&v=4",
+    image: "https://avatars.githubusercontent.com/u/5100938?s=100&v=4",
     url: "https://github.com/bmiddha",
     description: (
       <>
@@ -40,7 +38,7 @@ const sysadmins = [
   },
   {
     name: "Chase Lee",
-    image: "https://avatars.githubusercontent.com/u/1387910?s=200&v=4",
+    image: "https://avatars.githubusercontent.com/u/1387910?s=100&v=4",
     url: "https://github.com/clee231",
     description: (
       <>
@@ -52,8 +50,8 @@ const sysadmins = [
   },
   {
     name: "Christian Bingman",
-    image: "https://avatars.githubusercontent.com/u/42191425?s=200&v=4",
-    url: "https://github.com/ChristianBingman",
+    image: "https://git.christianbingman.com/avatar/19c1437930a1e9db7482a581fc45f60a?size=100",
+    url: "https://git.christianbingman.com/ChristianBingman",
     description: <>The new guy</>,
   },
 ];
@@ -73,42 +71,40 @@ export const HomepageProfiles: React.FC<HomepageOfficerProps> = () => (
   <>
     <div className="container">
       <div className="row">
-        <div className={clsx("col text--center", styles.title)}>
+        <div className="col text--center" style={{ color: "var(--ifm-color-primary)" }}>
           <h2>Meet the Board</h2>
         </div>
       </div>
     </div>
-    {sections.map((section, sectionKey) => (
-      <>
-        <section className={clsx(styles.profile_section)} key={sectionKey}>
-          <div className="container">
-            <div className="row">
-              <div className={clsx("col text--center", styles.profile_section_title)}>
-                <h3>{section.name}</h3>
-              </div>
-            </div>
-            <div className="row">
-              {section.profiles.map((profile, key) => (
-                <div className={clsx("col")} key={key}>
-                  <div className="text--center">
-                    <a href={profile.url} className={clsx(styles.profile_avatar)}>
-                      <img src={profile.image} alt={`Avatar of ${profile.name}`} />
-                    </a>
-                  </div>
-                  <div className={clsx("text--center padding-horiz--md", styles.profile_name)}>
-                    <h3>
-                      <a href={profile.url} target="_blank" rel="noopener noreferrer">
-                        {profile.name}
-                      </a>
-                    </h3>
-                    <p>{profile.description}</p>
-                  </div>
-                </div>
-              ))}
+    {sections.map((section, sectionIndex) => (
+      <section key={`${section.name}-${sectionIndex}`}>
+        <div className="container">
+          <div className="row">
+            <div className="col text--center margin-bottom--xs">
+              <h3>{section.name}</h3>
             </div>
           </div>
-        </section>
-      </>
+          <div className="row">
+            {section.profiles.map((profile, profileIndex) => (
+              <div className="col avatar avatar--vertical margin-bottom--lg" key={`${profile.name}-${profileIndex}`}>
+                <div className="margin-bottom--sm">
+                  <a href={profile.url} className="avatar__photo-link avatar__photo avatar__photo--xl">
+                    <img src={profile.image} alt={`Avatar of ${profile.name}`} />
+                  </a>
+                </div>
+                <div className="avatar__intro">
+                  <div className="avatar__name">
+                    <a href={profile.url} target="_blank" rel="noopener noreferrer">
+                      {profile.name}
+                    </a>
+                  </div>
+                  <small className="avatar__subtitle">{profile.description}</small>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     ))}
   </>
 );
