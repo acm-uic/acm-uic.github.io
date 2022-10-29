8ae683bb3f14892ddbbc3bdf6acf7f409d0c8357
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2022-10-29 13:17:21 -0700

    stop shimmering animation when events fail to load

--- src/components/Shimmer/ShimmerLine.tsx
+++ src/components/Shimmer/ShimmerLine.tsx
@@ -2,4 +2,14 @@ import * as React from "react";
 import clsx from "clsx";
 import styles from "./ShimmerLine.module.css";
 
-export const ShimmerLine: React.FC = () => <span className={clsx(styles.lines, styles.shine)} />;
+export interface ShimmerLineProps {
+  /**
+   * Show shimmer animation
+   * @default true
+   */
+  animate?: boolean;
+}
+
+export const ShimmerLine: React.FC<ShimmerLineProps> = ({ animate = true }) => (
+  <span className={clsx(styles.lines, styles.shine, animate && styles.shineAnimation)} />
+);
