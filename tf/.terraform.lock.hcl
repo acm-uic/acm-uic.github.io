ce1d5a419ded5aaef9ff4b592222ece521a3e06e
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2023-03-28 22:24:08 -0700

    deploy typesense with terraform

---
+++ tf/.terraform.lock.hcl
@@ -0,0 +1,66 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "3.49.0"
+  constraints = "3.49.0"
+  hashes = [
+    "h1:g4h3JYLbkZ7030/PcW5n7WVkw0XjjuSV5jvp7gMP5bw=",
+    "zh:09ca3159e6dfcfe7062f42c817806ad90e6b02fad4078bbb4d6c59f7bd612051",
+    "zh:1ad159bc759d9bbf8de8408f483f7a58a5067735004d18fd86b83272ebf72522",
+    "zh:1c98738713b5beb73f7400172c16abc5c96649c6a158bd7ae560fd46286a5633",
+    "zh:357acfccd85a21ae9c69c9b0ac2fe2452422ce88a0b0e08e27bcd1bac3c170f0",
+    "zh:4a9383f1f801cba9bc3450016e2d7cbe5c7eb14b6c3c333d2bacc88bc8a14ff2",
+    "zh:96c99b3bf016ad6a80bc2a24faeac4bd2b92824f9737cc88661396dc88e94fbb",
+    "zh:ab785827177c9d3977416614a8d351b4fce0d6029c2912a5cacc7efe9592c060",
+    "zh:cdadf38a655717942c6e3fe0508dac7094b9ea56373246b5d83afbbacbbb0be9",
+    "zh:e9f19d1173ad8ddc9248bf9558a31a0cd00651ea04c55ba9c58e52542ae39a68",
+    "zh:ef83d8077cc8fc9ad2d06125b3350337f950921bbb946aeea064c2442afa5a90",
+    "zh:f1031745dbf24760066a8d355f0922728eeb37beff130fba976eeab951eec22e",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/integrations/github" {
+  version     = "5.18.3"
+  constraints = "5.18.3"
+  hashes = [
+    "h1:Z/0vjFX80YzM3Oeq0mBbn4XYwb1POggjsu3RVQcbjNc=",
+    "zh:050b37d96628cb7451137755929ca8d21ea546bc46d11a715652584070e83ff2",
+    "zh:053051061f1b7f7673b0ceffac1f239ba28b0e5b375999206fd39976e85d9f2b",
+    "zh:0c300a977ca66d0347ed62bb116fd8fc9abb376a554d4c192d14f3ea71c83500",
+    "zh:1d5a1a5243eba78819d2f92ff2d504ebf9a9008a6670fb5f5660f44eb6a156d8",
+    "zh:a13ac15d251ebf4e7dc40acb0e40df066f443f4c7799186a29e2e44addc7d8e7",
+    "zh:a316d94b885953c036ebc9fba64a23da93974746bc3ac9d207462a6f02d44540",
+    "zh:a658a00373bff5979cc227052c693cbde8ca4c8f9fef1bc8094a3516f2e2a96d",
+    "zh:a7bfc6ad8465d5dc11b6f19d6805364de87fffe27622bb4f37da2319bb1c4956",
+    "zh:d7379a76861f1a6bfc36eca7a20f1f477711247563b105744d69d7bd1f365fad",
+    "zh:de1cd959fd4821248e8d21570601193408648474e74f49597f1d0c43185a4ab7",
+    "zh:e0b281240dd6f2aa405b2d6fe329bc15ab877161affe163fb150d1efca2fccdb",
+    "zh:e372c171358757a983d7aa878abfd05a84484fb4d22167e45c9c1267e78ed060",
+    "zh:f6d3116526030b3f6905f530cd6c04b23d42890d973fa2abe10ce9c89cb1db80",
+    "zh:f99eec731e03cc6a28996c875bd435887cd7ea75ec07cc77b9e768bb12da2227",
+  ]
+}
+
+provider "registry.terraform.io/linyinfeng/shell" {
+  version     = "1.7.12"
+  constraints = "1.7.12"
+  hashes = [
+    "h1:s6dxJj1htfz0i7i+VWE9inu7qRPkJc8w6GgNDpXlpY8=",
+    "zh:089de5cff698b5e4d2ab4a6fbc67d90c1a71efd1c8e666071cd8b81c28c14326",
+    "zh:1cb6b16d4edf3a14a3fd293b4287cfe1f7372847db3fa570f73ebb9e32a74492",
+    "zh:23c40760244613932dc5733fc945ccad1bf1371585552cbe3e294a74442e092f",
+    "zh:32b921f25e6e3cef299ba88d60621e5d77a2eee088b998769c11a6779b43d112",
+    "zh:4cf97d63e43838ad74a778199a9693cc354e54b742d14829734b8f79090b9b2b",
+    "zh:51ddd1a63dcabfad61f14e99ebc0b5a2d819a1b43b1962471ce73d3d0ed32976",
+    "zh:7bf11b78d09650d7d8d430d637b8d81b8a1ce6b1947177f910467609d735cb8b",
+    "zh:9a6b515f70df34a3cfc803f8221766a4edcff0c0c537238f4c1dbfeef81233c8",
+    "zh:9ca235f5e695d391990887bbfee2b60ed3702dffa33f8696fa5ef3c84589d054",
+    "zh:a20ddfd9853186a2717910698a905bbb2738da6e2e9b19abbddce7b52644cd15",
+    "zh:cbfc3299a0b874339972a22b3044bc330c0e2ab6ac6cf1ea57b8c0c60f4f6e1f",
+    "zh:e7a2dd66372deb5a346366fcbf14e5e6728086b909c69519a949e42196bac0d8",
+    "zh:f0ecd3a4b9a03cac1ba90a9bccceed0ed7f16d85472a7d6221bde031ce639e94",
+    "zh:f7d39bb21977cb15c8d77d6512982eeba61464242f5a1c88d04babc74bfc7fc7",
+  ]
+}
---
+++ tf/typesense.tf
@@ -0,0 +1,221 @@
+locals {
+  gh_repo = "acm-uic.github.io"
+}
+
+data "azurerm_resource_group" "typesense_resource_group" {
+  name = var.typesense_rg
+}
+
+resource "azurerm_container_app" "typesense_container_app" {
+  name                         = "typesense-container-app"
+  container_app_environment_id = azurerm_container_app_environment.typesense_container_app_env.id
+  resource_group_name          = data.azurerm_resource_group.typesense_resource_group.name
+  revision_mode                = "Single"
+  ingress {
+    allow_insecure_connections = false
+    external_enabled           = true
+    target_port                = 8108
+    transport                  = "auto"
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
+    }
+  }
+  secret {
+    name  = "api-key"
+    value = var.typesense_api_key
+  }
+  template {
+    max_replicas    = 1
+    min_replicas    = 1
+    revision_suffix = "one"
+    container {
+      args    = []
+      command = []
+      cpu     = 1
+      image   = "docker.io/typesense/typesense:0.24.0"
+      memory  = "2Gi"
+      name    = "typesense-acm-website"
+      env {
+        name  = "TYPESENSE_DATA_DIR"
+        value = "/data"
+      }
+      env {
+        name        = "TYPESENSE_API_KEY"
+        secret_name = "api-key"
+      }
+      liveness_probe {
+        initial_delay    = 10
+        interval_seconds = 10
+        path             = "/health"
+        port             = 8108
+        transport        = "HTTP"
+      }
+      readiness_probe {
+        interval_seconds = 10
+        path             = "/health"
+        port             = 8108
+        transport        = "HTTP"
+      }
+      volume_mounts {
+        name = "typesense-volume"
+        path = "/data"
+      }
+    }
+    volume {
+      name         = "typesense-volume"
+      storage_name = azurerm_container_app_environment_storage.typesense_container_app_env_storage.name
+      storage_type = "AzureFile"
+    }
+  }
+}
+
+resource "azurerm_container_app_environment" "typesense_container_app_env" {
+  name                       = "typesense-container-app-env"
+  location                   = data.azurerm_resource_group.typesense_resource_group.location
+  resource_group_name        = data.azurerm_resource_group.typesense_resource_group.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.typesense_log_workspace.id
+}
+
+resource "azurerm_container_app_environment_storage" "typesense_container_app_env_storage" {
+  name                         = "typesense-container-app-storage"
+  access_mode                  = "ReadWrite"
+  container_app_environment_id = azurerm_container_app_environment.typesense_container_app_env.id
+  account_name                 = azurerm_storage_account.typesense_storage_account.name
+  share_name                   = azurerm_storage_share.typesense_storage_share.name
+  access_key                   = azurerm_storage_account.typesense_storage_account.primary_access_key
+}
+
+resource "azurerm_storage_account" "typesense_storage_account" {
+  name                     = "typesensestorage"
+  account_kind             = "FileStorage"
+  account_replication_type = "LRS"
+  account_tier             = "Premium"
+  location                 = data.azurerm_resource_group.typesense_resource_group.location
+  resource_group_name      = data.azurerm_resource_group.typesense_resource_group.name
+}
+
+resource "azurerm_storage_share" "typesense_storage_share" {
+  name                 = "typesense-share"
+  quota                = 100
+  storage_account_name = azurerm_storage_account.typesense_storage_account.name
+}
+
+resource "azurerm_log_analytics_workspace" "typesense_log_workspace" {
+  name                = "typesense-logs"
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+  location            = data.azurerm_resource_group.typesense_resource_group.location
+  resource_group_name = data.azurerm_resource_group.typesense_resource_group.name
+}
+
+resource "shell_sensitive_script" "github_secret_typesense_search_only_api_key" {
+  triggers = {
+    when_value_changed = azurerm_container_app.typesense_container_app.latest_revision_fqdn
+    when_value_changed = azurerm_container_app.typesense_container_app.id
+  }
+  environment = {
+    REPO = local.gh_repo
+  }
+  sensitive_environment = {
+    GITHUB_TOKEN                  = var.github_token
+    TYPESENSE_SEARCH_ONLY_API_KEY = shell_sensitive_script.typesense_search_only_api_key.output["value"]
+  }
+  lifecycle {
+    replace_triggered_by = [
+      shell_sensitive_script.typesense_search_only_api_key
+    ]
+  }
+  lifecycle_commands {
+    create = <<-EOF
+      gh secret set TYPESENSE_SEARCH_ONLY_API_KEY -b $TYPESENSE_SEARCH_ONLY_API_KEY -R acm-uic/acm-uic.github.io
+      EOF
+    delete = <<-EOF
+      gh secret remove TYPESENSE_SEARCH_ONLY_API_KEY -R acm-uic/acm-uic.github.io
+      EOF
+  }
+}
+
+resource "shell_sensitive_script" "github_secret_typesense_host" {
+  triggers = {
+    when_value_changed = azurerm_container_app.typesense_container_app.latest_revision_fqdn
+    when_value_changed = azurerm_container_app.typesense_container_app.id
+  }
+  environment = {
+    REPO           = local.gh_repo
+    TYPESENSE_HOST = azurerm_container_app.typesense_container_app.latest_revision_fqdn
+  }
+  sensitive_environment = {
+    GITHUB_TOKEN = var.github_token
+  }
+  lifecycle {
+    replace_triggered_by = [
+      azurerm_container_app.typesense_container_app
+    ]
+  }
+  lifecycle_commands {
+    create = <<-EOF
+      gh secret set TYPESENSE_HOST \
+        -R acm-uic/acm-uic.github.io \
+        -b $TYPESENSE_HOST
+      EOF
+    delete = <<-EOF
+      gh secret remove TYPESENSE_HOST \
+        -R acm-uic/acm-uic.github.io
+      EOF
+  }
+}
+
+resource "shell_sensitive_script" "typesense_search_only_api_key" {
+  triggers = {
+    when_value_changed = azurerm_container_app.typesense_container_app.latest_revision_fqdn
+    when_value_changed = azurerm_container_app.typesense_container_app.id
+    when_value_changed = var.typesense_api_key
+  }
+  environment = {
+    TYPESENSE_HOST  = azurerm_container_app.typesense_container_app.latest_revision_fqdn
+    COLLECTION_NAME = var.typesense_collection_name
+  }
+  depends_on = [
+    azurerm_container_app.typesense_container_app
+  ]
+  sensitive_environment = {
+    TYPESENSE_API_KEY = var.typesense_api_key
+  }
+  lifecycle {
+    replace_triggered_by = [
+      azurerm_container_app.typesense_container_app
+    ]
+  }
+  lifecycle_commands {
+    create = <<-EOF
+      until curl --silent "https://$TYPESENSE_HOST/health" | grep -q '"ok":true'; do sleep 5; done;
+      curl --silent "https://$TYPESENSE_HOST/keys" \
+        -X POST \
+        -H "X-TYPESENSE-API-KEY: $TYPESENSE_API_KEY" \
+        -H 'Content-Type: application/json' \
+        -d "{\"description\":\"search-only-api-key-$COLLECTION_NAME\",\"actions\": [\"documents:search\"], \"collections\": [\"$COLLECTION_NAME\"]}" \
+        | jq '{id, value}'
+      EOF
+    delete = <<-EOF
+      IN=$(cat)
+      ID=$(echo $IN | jq -r .id)
+      until curl --silent "https://$TYPESENSE_HOST/health" | grep -q '"ok":true'; do sleep 5; done;
+      curl --silent "https://$TYPESENSE_HOST/keys/$ID" \
+        -X DELETE \
+        -H "X-TYPESENSE-API-KEY: $TYPESENSE_API_KEY"
+      EOF
+  }
+}
+
+#resource "github_actions_secret" "typesense_search_only_api_key" {
+#  repository      = local.gh_repo
+#  secret_name     = "TYPESENSE_SEARCH_ONLY_API_KEY"
+#  encrypted_value = shell_sensitive_script.typesense_search_only_api_key.output["value"]
+#}
+#
+#resource "github_actions_secret" "typesense_host" {
+#  repository      = local.gh_repo
+#  secret_name     = "TYPESENSE_HOST"
+#  encrypted_value = azurerm_container_app.typesense_container_app.ingress[0].fqdn
+#}
